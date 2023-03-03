0f57a35f7797c4864fb4ca92f96b9ec23498cc66
Your Name <you@example.com>
2023-03-03 16:52:51 +0700

    Init project

--- backend/src/common/config/app.config.ts
+++ backend/src/common/config/app.config.ts
@@ -4,6 +4,6 @@ export default registerAs('app', () => ({
   port: process.env.PORT || 3000,
   isSwagger: process.env.IS_SWAGGER || true,
   uploadDir: process.env.UPLOAD_DIR || '',
-  i18nFolder: process.env.I18N_FOLDER || '/home/duc/tconnect/frontend/src/locales',
+  i18nFolder: process.env.I18N_FOLDER || '/home/duc/tconnect_new/frontend/src/locales',
   defaultI18n: process.env.DEFAULT_I18N || 'vi-VN',
 }));
