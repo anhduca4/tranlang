2f72f1361077d69b05f059601e9d5a7c4a74f500
Your Name <you@example.com>
2025-12-19 17:42:59 +0700

    test

---
+++ hellochiem.java
@@ -0,0 +1,31 @@
+public class hellochiem {
+    public static void main(String[] args) {
+        
+    }
+
+    static void printGreeting() {
+        // Removed: System.out.println("Hello, Chiem!");
+    }
+
+    static void printNumber() {
+        // Removed: System.out.println("This is line 2.");
+    }
+
+    static void printPattern() {
+        // Removed: System.out.println("***");
+    }
+    static void printSum() {
+        int sum = 7 + 8;
+        // Removed: System.out.println("The sum of 7 and 8 is: " + sum);
+    }
+
+    static void printReverse() {
+        String word = "Hello";
+        String rev = new StringBuilder(word).reverse().toString();
+        // Removed: System.out.println("Reverse of Hello: " + rev);
+    }
+
+    static void printBye() {
+        // Removed: System.out.println("Goodbye!");
+    }
+}
