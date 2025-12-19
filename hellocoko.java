9c65a1048f546e15bbbd8c9221b95433a8acdc81
Your Name <you@example.com>
2025-12-19 17:58:02 +0700

    test

---
+++ hellocoko.java
@@ -0,0 +1,20 @@
+public class hellocoko {
+    // Entry point for the program
+    public static void main(String[] args) {
+        printWelcome();
+        printLine();
+        printNumber();
+        printBye();
+    }
+
+    // Print a number
+    public static void printNumber() {
+        System.out.println("Number: " + 123);
+    }
+
+    // Print bye message
+    public static void printBye() {
+        System.out.println("Goodbye!");
+    }
+    println("hello")
+}
