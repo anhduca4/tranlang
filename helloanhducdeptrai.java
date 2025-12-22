6051a935a0711393dd5b7211a5a787f2f076843d
Your Name <you@example.com>
2025-12-22 09:49:01 +0700

    tét

---
+++ helloanhducdeptrai.java
@@ -0,0 +1,6 @@
+public class Helloanhducdeptrai {
+    public static void main(String[] args) {
+        System.out.println("helloanhducdeptrai");
+        String message = "Java coding by anhducdeptrai";
+    }
+}
