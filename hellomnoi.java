e447d6fca7350efce82d3093c2cfcf8b620b61d4
Your Name <you@example.com>
2025-12-22 09:01:35 +0700

    tét

---
+++ hellomnoi.java
@@ -0,0 +1,8 @@
+public class hellomnoi {
+    public static void main(String[] args) {
+        System.out.println("hellomnoi");
+        int number = 123;
+        System.out.println("The number is: " + number);
+        System.out.println("Double the number is: " + (number * 2));
+    }
+}
