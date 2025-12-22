440239b60019789008de57d6d455673eec55380f
Your Name <you@example.com>
2025-12-22 10:22:41 +0700

    test

---
+++ helloanhemoi.java
@@ -0,0 +1,8 @@
+public class helloanhemoi {
+    public static void main(String[] args) {
+        System.out.println("helloanhemoi");
+        System.out.println("This is line 2");
+        System.out.println("This is line 3");
+        System.out.println("This is line 4");
+    }
+}
