7304737406fa1492282cc414cd109f39bf86ed09
Your Name <you@example.com>
2025-12-22 11:16:41 +0700

    a

--- hello1.java
+++
@@ -1,6 +0,0 @@
-public class hello1 {
-    public static void main(String[] args) {
-        System.out.println("hello1");
-        System.out.println("This is line 2");
-    }
-}
--- hello2.java
+++
@@ -1,7 +0,0 @@
-// This is Hello2 Java class example
-public class Hello2 {
-    public static void main(String[] args) {
-        System.out.println("Hello from Hello2!");
-        int number = 3; // Sample variable assignment
-    }
-}
