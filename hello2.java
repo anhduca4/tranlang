91fd7981d44c60ad66f5c55df4efc32bc2f508e2
Your Name <you@example.com>
2025-12-23 13:49:13 +0700

    test

---
+++ hello2.java
@@ -0,0 +1,8 @@
+public class Hello2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello from hello7!");
+        System.out.println("This is line 1 of the addition.");
+        System.out.println("This is line 3 of the addition.");
+    }
+}
