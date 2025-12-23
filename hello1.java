d41925fb425b0758d43c3d81ed57ede788f79eb7
Your Name <you@example.com>
2025-12-23 11:03:50 +0700

    test

---
+++ hello1.java
@@ -0,0 +1,8 @@
+public class hello1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello from hello7!");
+        int a = 5;
+        int b = 10;
+    }
+}
