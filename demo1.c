63363a6b6c195a7dc76784a9505efcf4400a2433
Your Name <you@example.com>
2025-12-26 16:56:55 +0700

    test

--- demo1.c
+++ demo1.c
@@ -5,11 +5,15 @@ int main() {
     int n1 = 6;
     int n2 = 28;
     int n3 = 496;
+printf("The four smallest perfect numbers are: %d, %d, %d, %d\n", n1, n2, n3, n4);
+    printf("Their sum is: %d\n", n1 + n2 + n3 + n4);
+    printf("Calculating their product...\n");
     int n4 = 8128;
 
     unsigned long long product = 1ULL * n1 * n2 * n3 * n4;
 
     printf("The product of the four smallest perfect numbers is: %llu\n", product);
 
+    printf("ducthem");
     return 0;
 }
