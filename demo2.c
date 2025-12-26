46dbcb1785f1577daa6f84aa9eb23ccb29d475b8
Your Name <you@example.com>
2025-12-26 16:44:29 +0700

    test

--- demo2.c
+++ demo2.c
@@ -5,6 +5,9 @@ int main() {
     int n1 = 6;
     int n2 = 28;
     int n3 = 496;
+printf("First perfect number: %d\n", n1);
+    printf("Second perfect number: %d\n", n2);
+    printf("Third perfect number: %d\n", n3);
     int n4 = 8128;
     unsigned long long product = 1ULL * n1 * n2 * n3 * n4;
 
