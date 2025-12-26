b31825c053caed240e73a872e30c02731ff9fe97
Your Name <you@example.com>
2025-12-26 16:27:53 +0700

    test

--- demo1.c
+++ demo1.c
@@ -10,6 +10,13 @@ int main() {
     // Because the result will be large, use long long for product
     long long product = 1LL * num1 * num2 * num3 * num4;
 
+    // Additional 5 print lines as requested
+    printf("The first perfect number: %d\n", num1);
+    printf("The second perfect number: %d\n", num2);
+    printf("The third perfect number: %d\n", num3);
+    printf("The fourth perfect number: %d\n", num4);
+    printf("Multiplying them: %d * %d * %d * %d\n", num1, num2, num3, num4);
+
     printf("The product of the four smallest perfect numbers (%d, %d, %d, %d) is: %lld\n", num1, num2, num3, num4, product);
 
     return 0;
