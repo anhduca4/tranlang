d479af4f63f5f4aac5686f00ac48580ee9f64d8e
Your Name <you@example.com>
2025-12-26 15:54:31 +0700

    tttt

--- perfect_product.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    // The four smallest perfect numbers
-    int n1 = 6, n2 = 28, n3 = 496, n4 = 8128;
-    long long product = (long long)n1 * n2 * n3 * n4;
-    printf("The product of the four smallest perfect numbers is: %lld\n", product);
-    return 0;
-}
