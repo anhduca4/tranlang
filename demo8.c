8a139da3dc3917433959f6e5857dba9c15a3bec2
Your Name <you@example.com>
2025-12-27 10:22:27 +0700

    test

--- demo8.c
+++ demo8.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main() {
-    // The 4 smallest perfect numbers
+    srand(time(NULL));
     int a = 6;
-    int b= 0;
+    int b = 28;
+    int c = rand() % 100 + 1; // random between 1-100
+    int d = rand() % 100 + 1;
+    int e = rand() % 100 + 1;
 
-    unsigned long long product = (unsigned long long)a * b;
+    unsigned long long product = (unsigned long long)a * b * c * d * e;
 
-    printf("The product of the two smallest perfect numbers (%d, %d) is: %llu\n", a, b, product);
+    printf("The product of numbers a=%d, b=%d, c=%d, d=%d, e=%d is: %llu\n", a, b, c, d, e, product);
 
     return 0;
 }
