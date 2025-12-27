66a7c903a142178a42f6e9d9312a09766b9e765d
Your Name <you@example.com>
2025-12-27 10:14:34 +0700

    test

---
+++ demo6.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // The first four perfect numbers are:
+
+
+    printf("The product of the four smallest perfect numbers is: %llu\n", product);
+    return 0;
+}
