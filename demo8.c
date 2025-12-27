ae66ea1b949fa0bb8e4a4ccd8c78117d7da0384b
Your Name <you@example.com>
2025-12-27 10:23:42 +0700

    test

--- demo8.c
+++ demo8.c
@@ -6,13 +6,10 @@ int main() {
     srand(time(NULL));
     int a = 6;
     int b = 28;
-    int c = rand() % 100 + 1; // random between 1-100
-    int d = rand() % 100 + 1;
-    int e = rand() % 100 + 1;
 
-    unsigned long long product = (unsigned long long)a * b * c * d * e;
+    unsigned long long product = (unsigned long long)a * b ;
 
-    printf("The product of numbers a=%d, b=%d, c=%d, d=%d, e=%d is: %llu\n", a, b, c, d, e, product);
+    printf("The product of numbers a=%d, b=%d, is: %llu\n", a, b, product);
 
     return 0;
 }
