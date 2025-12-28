e591d6e2e61340689354f0b415d75297193ec051
Your Name <you@example.com>
2025-12-28 17:38:08 +0700

    test

--- demo1.c
+++ demo1.c
@@ -1,31 +1,19 @@
 #include <stdio.h>
 
-// Function to check if a number is perfect
-int is_perfect(int n) {
-    int sum = 1;
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            sum += i;
-            if (i != n / i)
-                sum += n / i;
-        }
-    }
-    return (n != 1) && (sum == n);
-}
-
 int main() {
-    int found = 0;
-    int num = 2;
-    long long product = 1;
-    printf("The first 4 perfect numbers are: ");
-    while (found < 4) {
-        if (is_perfect(num)) {
-            printf("%d ", num);
-            product *= num;
-            found++;
-        }
-        num++;
-    }
-    printf("\\nTheir product is: %lld\\n", product);
+    // The four smallest perfect numbers
+    int perfect1 = 6;
+    int perfect2 = 28;
+    int perfect3 = 496;
+    int perfect4 = 8128;
+
+    unsigned long long product = 1;
+    product *= perfect1;
+    product *= perfect2;
+    product *= perfect3;
+    product *= perfect4;
+
+    printf("The product of the four smallest perfect numbers (%d, %d, %d, %d) is: %llu\n", 
+        perfect1, perfect2, perfect3, perfect4, product);
     return 0;
 }
