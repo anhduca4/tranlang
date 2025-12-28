b6b071ea29f315fb84b21596c410973ac58bb5eb
Your Name <you@example.com>
2025-12-28 16:21:25 +0700

    test

--- demo4.c
+++ demo4.c
@@ -5,6 +5,7 @@ int main() {
     int n1 = 6, n2 = 28, n3 = 496, n4 = 8128;
     unsigned long long product = (unsigned long long)n1 * n2 * n3 * n4;
 
+    printf("The four smallest perfect numbers are: %d, %d, %d, %d\\n", n1, n2, n3, n4);
     printf("The product of the four smallest perfect numbers is: %llu\\n", product);
 
     return 0;
