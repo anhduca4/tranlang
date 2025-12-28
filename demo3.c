38d93ca194c6dee43b196869c0e7360c4fa88f3b
Your Name <you@example.com>
2025-12-28 17:42:52 +0700

    test

---
+++ demo3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // The four smallest perfect numbers
+    int perfect1 = 6;
+    int perfect2 = 28;
+    int perfect3 = 496;
+    int perfect4 = 8128;
+
+    unsigned long long product = 1;
+    product *= perfect1;
+    product *= perfect2;
+    product *= perfect3;
+    product *= perfect4;
+
+    printf("The four smallest perfect numbers are: %d, %d, %d, %d\n", perfect1, perfect2, perfect3, perfect4);
+    printf("Perfect number 3: %d\n", perfect3);
+    printf("The product of the four smallest perfect numbers is: %llu\n", product);
+
+    
+    return 0;
+}
