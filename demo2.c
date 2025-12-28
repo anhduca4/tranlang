33f5b9b17d6700612375753636dc47e84698773d
Your Name <you@example.com>
2025-12-28 15:34:24 +0700

    test

---
+++ demo2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // The four smallest perfect numbers
+    int nums[] = {6, 28, 496, 8128};
+    unsigned long long product = 1;
+    for(int i = 0; i < 4; ++i) {
+        product *= nums[i];
+    }
+    printf("The product of the 4 smallest perfect numbers is: %llu\n", product);
+    return 0;
+}
