3e239b2a6681b90b6c8e02a0f4861e31e9481f15
Your Name <you@example.com>
2025-12-29 17:44:23 +0700

    test

--- demo1.c
+++ demo1.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
+// The first 10 known perfect numbers (as strings for large values)
+const char* perfect_numbers[10] = {
+    "6",
+    "28",
+    "496",
+    "8128",
+    "33550336",
+    "8589869056",
+    "137438691328",
+    "2305843008139952128",
+    "265252859812191058636308480000000",
+    "191561942608236107294793378084303638130997321548169216"
+};
+
 int main() {
-    // The four smallest perfect numbers
-    int perfect1 = 6;
-    int perfect2 = 28;
-    int perfect3 = 496;
-    int perfect4 = 8128;
-
-    unsigned long long product = (unsigned long long)perfect1 * perfect2 * perfect3 * perfect4;
-
-    printf("The product of the four smallest perfect numbers is: %llu\n", product);
-
+    printf("The 10 smallest perfect numbers:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%2d: %s\n", i + 1, perfect_numbers[i]);
+    }
     return 0;
 }
