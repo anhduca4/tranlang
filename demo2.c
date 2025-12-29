ec619bfdd4d8179f3e66c876b90636dea1586313
Your Name <you@example.com>
2025-12-29 15:24:48 +0700

    a

---
+++ demo2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // The four smallest perfect numbers
+    int p1 = 6, p2 = 28, p3 = 496, p4 = 8128;
+    unsigned long long product = 1ULL * p1 * p2 * p3 * p4;
+
+    printf("demo1");
+    return 0;
+}
